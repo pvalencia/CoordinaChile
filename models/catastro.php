3b259a9d2435850ffa2a1ebedd0001ba38c83eda
pvalencia <pvalencia@e6754530-29f6-11df-b68c-f1a064176c42>
2010-03-14 18:16:12 +0000

    Validación de Catastro

--- models/catastro.php
+++ models/catastro.php
@@ -4,26 +4,112 @@ class Catastro extends AppModel {
 	var $name = 'Catastro';
 	var $validate = array(
 		'localidad_id' => array('numeric'),
-		'nombre_contacto' => array('notempty'),
-		'email_contacto' => array('email'),
-		'telefono_contacto' => array('alphaNumeric'),
-		'danos_graves_fisicos' => array('numeric'),
-		'danos_graves_psicologicos' => array('numeric'),
-		'personas_con_discapacidad' => array('numeric'),
-		'enfermedades_cronicas' => array('numeric'),
-		'embarazadas' => array('numeric'),
-		'menores' => array('numeric'),
-		'casas_destruidas' => array('numeric'),
-		'casas_remocion_escombros' => array('numeric'),
-		'casas_evaluacion_estructural' => array('numeric'),
-		'sistema_excretas' => array('numeric'),
-		'agua' => array('numeric'),
-		'ropa' => array('numeric'),
-		'abrigo' => array('numeric'),
-		'colchoneta' => array('numeric'),
-		'aseo_personal' => array('numeric'),
-		'aseo_general' => array('numeric'),
-		'combustible' => array('numeric')
+		'nombre_contacto' => array(
+			'novacio' => array(
+				'rule' => 'notempty',
+				'required' => true,
+				'message' => 'Debes especificar el nombre y apelllido de contacto.'
+			),
+		),
+		'email_contacto' => array(
+			'email' => array(
+				'rule' => 'email',
+				'required' => true,
+				'message' => 'Debes especificar un correo electr&oacute;nico v&aacute;lido.',
+			),
+		),
+		'telefono_contacto' => array(
+			'telefono' => array(
+				'rule' => 'alphaNumeric',
+				'required' => true,
+				'message' => 'Debes especificar un n&uacute;mero de tel&eacute;fono de contacto v&aacute;lido.',
+			),
+		),
+		'danos_graves_fisicos' => array(
+			'rule' => 'numeric',
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'danos_graves_psicologicos' => array(
+			'rule' => 'numeric',
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'personas_con_discapacidad' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'enfermedades_cronicas' => array(
+			'rule' => 'numeric',
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'embarazadas' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'menores' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'casas_destruidas' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'casas_remocion_escombros' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'casas_evaluacion_estructural' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'sistema_excretas' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'agua' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'ropa' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'abrigo' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'colchoneta' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'aseo_personal' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'aseo_general' => array(
+			'rule' => 'numeric'
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		),
+		'combustible' => array(
+			'rule' => 'numeric',
+			'allowEmpty' => true,
+			'message' => 'Debes escribir un n&uacute;mero.'
+		)
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
