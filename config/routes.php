126e304e220dea1e18088e08e0a4fb3304c43e95
Pedro Valencia <pedro@vostro-ubuntu.(none)>
2010-03-23 19:11:29 -0300

    cambiado el default a index en todos los controladores

--- config/routes.php
+++ config/routes.php
@@ -3,10 +3,7 @@ Router::connect('/', array('controller' => 'comunas', 'action' => 'mapa'));
 
 
 Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
-Router::connect('/organizaciones', array('controller' => 'organizaciones', 'action' => 'todos'));
-Router::connect('/catastros', array('controller' => 'catastros', 'action' => 'todos'));
-Router::connect('/operativos', array('controller' => 'operativos', 'action' => 'todos'));
-Router::connect('/comunas', array('controller' => 'comunas', 'action' => 'todos'));
+
 Router::connect('/archivo/:id/:nombre', array('controller' => 'catastros', 'action' => 'bajar_archivo'), 
 										array('id' => '\d+', 'nombre' => '.+'));
 
